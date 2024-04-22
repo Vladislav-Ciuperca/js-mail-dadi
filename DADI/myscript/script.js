a078ab3c111983b3fdd9cd4684dd0929373c47e7
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-22 20:04:38 +0200

    copommit 3

---
+++ DADI/myscript/script.js
@@ -0,0 +1,27 @@
+// alert('ciaone')
+const umano = document.getElementById('umano');
+const computer = document.getElementById('computer');
+const vincitore = document.getElementById('vincitore');
+
+
+function azione() {
+    const ptumano = Math.floor(Math.random() * 6) + 1;
+    const ptcomputer = Math.floor(Math.random() * 6) + 1;
+    umano.innerHTML = ptumano;
+    computer.innerHTML = ptcomputer;
+
+    if (ptumano > ptcomputer) {
+        vincitore.innerHTML = 'vince umano'
+    }
+    else if (ptumano == ptcomputer) {
+        vincitore.innerHTML = 'pareggio , rilancia';
+    }
+    else {
+        vincitore.innerHTML = 'vince compuer';
+    }
+}
+
+
+
+
+
